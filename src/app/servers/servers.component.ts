bf0534365a5856aeb4088364a36493fd2b502ba0
Richard Memory <richardfmemory@gmail.com>
2018-05-03 10:31:03 -0600

    Add initial databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,18 +4,32 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector: [app-servers] is an attribute selector, <div app-servers>
   //selector: .app-servers is a class selector, <div class="app-server"
-  // templateUrl: './servers.component.html',
-  template: `
-            <app-server></app-server>
+  templateUrl: './servers.component.html',
+  // template: `
+  //           <app-server></app-server>
 
-            <app-server></app-server>`,
+  //           <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName='';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created. Sever name is ' + this.serverName;
+  }
+
+  onUpdateSeverName(event: any) {
+    this.serverName = event.target.value;
+  }
 }
